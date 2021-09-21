6eb6ce2af0cd3859b4e8a3aa226bc6fac09c7571
gisellevonbingen <gisellevonbingen@gmail.com>
2021-09-21 13:54:27 +0900

    Mixin Test

--- src/main/java/com/github/gisellevonbingen/mixin/ArmorTransluction.java
+++ src/main/java/com/github/gisellevonbingen/mixin/ArmorTransluction.java
@@ -20,15 +20,17 @@ import net.minecraft.util.ResourceLocation;
 @Mixin(BipedArmorLayer.class)
 public abstract class ArmorTransluction<T extends LivingEntity, M extends BipedModel<T>, A extends BipedModel<T>> {
 
-	@Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/client/renderer/entity/layers/BipedArmorLayer;renderModel(Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;IZLnet/minecraft/client/renderer/entity/model/BipedModel;FFFLnet/minecraft/util/ResourceLocation;)V", cancellable = true)
+	@Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/client/renderer/entity/layers/BipedArmorLayer;renderModel(Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;IZLnet/minecraft/client/renderer/entity/model/BipedModel;FFFLnet/minecraft/util/ResourceLocation;)V", cancellable = true, remap = false)
 	private void renderModel(MatrixStack p_241738_1_, IRenderTypeBuffer p_241738_2_, int p_241738_3_, boolean p_241738_5_, A p_241738_6_, float p_241738_8_, float p_241738_9_, float p_241738_10_, ResourceLocation armorResource, CallbackInfo info) {
 
-//		if (armorResource.equals(new ResourceLocation("boss_tools:textures/spacesuitmode__layer_1_head.png"))) {
+		System.out.println("hello?");
+		
+		if (armorResource.equals(new ResourceLocation("boss_tools:textures/spacesuitmode__layer_1_head.png"))) {
 			info.cancel();
 			System.out.println("com.github.gisellevonbingen.mixin");
 			IVertexBuilder ivertexbuilder = ItemRenderer.getArmorFoilBuffer(p_241738_2_, RenderType.entityTranslucent(armorResource), false, p_241738_5_);
 			p_241738_6_.renderToBuffer(p_241738_1_, ivertexbuilder, p_241738_3_, OverlayTexture.NO_OVERLAY, p_241738_8_, p_241738_9_, p_241738_10_, 1.0F);
-//		}
+		}
 
 	}
 
